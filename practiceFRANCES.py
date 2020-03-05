3a4393b43fca8c7237d6bc31dbabd99de8fd6540
Annie Watson <a.watson@Annies-MacBook-Pro.local>
2020-03-05 09:19:19 -0800

    added the multiplyRandom function

--- practiceFRANCES.py
+++ practiceFRANCES.py
@@ -4,15 +4,22 @@
 #######
 # Assignment Name: GitHub Practice - 03/03/2020 - 20 points
 import random
+import numpy as np
 
 def getNRandom(n):
 	'''takes in an integer and returns a list of n random integers between 1 and 10, inclusive'''
     for i in range(n):
         print random.randint(1,11)
 
-def multiplyRandom(numbers):
+def multiplyRandom(n):
 	'''takes in a list of n numbers and returns the product of the numbers'''
-    pass
+    nums = np.randint(low=None, high=None, size=n)
+    answer = 1
+    for i in range(nums):
+        answer = answer*i
+    return answer
+
+multiplyRandom(5)
 
 def main():
 	print(multiplyRandom(getNRandom(10))
